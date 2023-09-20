16b98aa34cacd1cabd2c7df9db89c9e44e9666a9
DamianKifuso <damian.mwangura@gmail.com>
2023-09-20 21:47:24 +0800

    update and add new files

--- employeeSQL/data_engineering_analysis.sql
+++ employeeSQL/data_engineering_analysis.sql
@@ -20,12 +20,14 @@ CREATE TABLE "dept_emp" (
 );
 
 -- Create Department manager table
-create table dept_manager (
-	dept_no VARCHAR(10) NOT NULL,
-	emp_no INT NOT NULL,
-	foreign key (emp_no) references employees (emp_no),
-    foreign key (dept_no) references departments (dept_no)
+CREATE TABLE dept_manager (
+    dept_no VARCHAR   NOT NULL,
+    emp_no INT   NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (dept_no, emp_no)
 );
+
 
 CREATE TABLE "employees" (
     "emp_no" INT  NOT NULL,
